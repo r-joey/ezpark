8d47288e429eac295753cb0c0bf551cfcbfb09e3
Joey Rapista <yeojrapista@gmail.com>
2025-06-03 22:21:33 +0800

    updated port for render to auto set the port

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from ezpark import create_app
 from ezpark.extensions import socketio
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
