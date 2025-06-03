23a76a5dc0dd5f9fb652b104d14a66bf6f0b4674
Joey Rapista <yeojrapista@gmail.com>
2025-06-03 22:03:19 +0800

    removed debug=True for production

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from ezpark import create_app
-from ezpark.extensions import socketio, db
+from ezpark.extensions import socketio
 
 app = create_app()
 
-if __name__ == '__main__': 
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5000)
